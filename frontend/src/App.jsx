a0c8c862156fd7f8309df3368c7329e280e67cd5
Bushra Patel <bpatel60@yahoo.com>
2024-07-24 16:22:22 -0400

    Comment out photos array and the photolist component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,34 +3,24 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = new Array(3);
-  for (let i = 0; i < photos.length; i++) {
-    photos[i] = sampleDataForPhotoListItem;
-  }
+  // const photos = new Array(3);
+  // for (let i = 0; i < photos.length; i++) {
+  //   photos[i] = sampleDataForPhotoListItem;
+  // }
 
-  const photoList = photos.map((photoData, index) => {
-    return (
-      <PhotoListItem key={photoData.id + index} photoData={photoData}/>
-      
-    )
-  });
+  // const photoList = photos.map((photoData, index) => {
+  //   return (
+  //     <PhotoListItem key={photoData.id + index} photoData={photoData}/>
+  //   )
+  // });
 
   return (
     <div className="App">
-      {photoList}
+      {/* {photoList} */}
+      {/* OR */}
+      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
     </div>
   );
 };
