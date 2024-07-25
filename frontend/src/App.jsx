ac447f49ecfc08193ad38fb7470c4bcf9eb1f89e
Bushra Patel <bpatel60@yahoo.com>
2024-07-25 18:30:59 -0400

    Add the TopNavigation and the PhotoList components to the HomeRoute. Add HomeRoute Component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 // import PhotoListItem from './components/PhotoListItem';
-import PhotoList from 'components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -20,9 +19,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <TopNavigation />
-      {/* {photoList} */}
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
