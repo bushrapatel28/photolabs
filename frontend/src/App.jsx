58a9bf09683e255f141ccb9ad76ef89ab6dc8690
Bushra Patel <bpatel60@yahoo.com>
2024-07-25 18:24:39 -0400

    Add the TopicList and the FavBadge components to their parent component which is TopNavigation. Add the TopNavigation component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // import PhotoListItem from './components/PhotoListItem';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       {/* {photoList} */}
       <PhotoList />
     </div>
