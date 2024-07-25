5c23a084d603971c8132b863918e24eea84d63b5
Bushra Patel <bpatel60@yahoo.com>
2024-07-25 18:39:32 -0400

    Cleanup by removing hard coded sample data from PhotoList and TopiList components. Replace the sample data with imported mock data for each of these components accordingly

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,30 +2,13 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
       {/* Insert React */}
-      {sampleDataForTopicList.map((topicData) => {
+      {topics.map((topicData) => {
         return (
           <TopicListItem 
             key={topicData.id}
