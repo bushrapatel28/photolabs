67acf4cd22efff7a3a89f6e5402c3add6d4e1d44
Bushra Patel <bpatel60@yahoo.com>
2024-07-26 15:27:53 -0400

    Add a check in PhotoFavButton to only call toggleFavorites if the photoId does not exist in favorites

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -9,7 +9,7 @@ function PhotoFavButton(props) {
   const handleClick = () => {
     // using prevState to avoid stale state - Same as setFavorite((prev) => prev ? false : true)
     setFavorite((favorite) => favorite ? false : true);
-    props.toggleFavorites(props.photoId);
+    !props.favorites.includes(props.photoId) && props.toggleFavorites(props.photoId);
   }
 
 
