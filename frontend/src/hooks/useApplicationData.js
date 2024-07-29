cec82b4b81d22706288fea293b4aaf100cb1a987
Bushra Patel <bpatel60@yahoo.com>
2024-07-29 17:35:51 -0400

    Define Actions object, remove useState and implement useReducer. Define a reducer function to take in the current state and the action for the condition check. Modify the helper functions inside useApplicationData to call the reducers dispatch functions inside the logic. Return the state and the dispatch functions

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import React, {useState, useReducer} from 'react';
+import React, { useReducer} from 'react';
 
 export const ACTIONS = {
   FAV_PHOTO_ADDED: "FAV_PHOTO_ADDED",
@@ -6,23 +6,26 @@ export const ACTIONS = {
   SET_PHOTO_DATA: "SET_PHOTO_DATA",
   SET_TOPIC_DATA: "SET_TOPIC_DATA",
   SELECT_PHOTO: "SELECT_PHOTO",
-  DISPLAY_PHOTO_DETAILS: "DISPLAY_PHOTO_DETAILS"
+  DISPLAY_PHOTO_DETAILS: "DISPLAY_PHOTO_DETAILS",
+  CLOSE_PHOTO_DETAILS: "CLOSE_PHOTO_DETAILS"
 }
 
 function reducer(state, action) {
   switch (action.type) {
-    case FAV_PHOTO_ADDED: 
-      return state.favorites = [...favorites, action.photoId];
-    case FAV_PHOTO_REMOVED: 
-      return state.favorites = favorites.filter(id => id !== action.photoId);
-    case SET_PHOTO_DATA: 
-      return state.photoInfo = action.selectedPhoto;
-    case SET_TOPIC_DATA: 
+    case ACTIONS.FAV_PHOTO_ADDED: 
+      return {...state, favorites: action.newFavs};
+    case ACTIONS.FAV_PHOTO_REMOVED: 
+      return {...state, favorites: action.newFavs};
+    case ACTIONS.SET_PHOTO_DATA: 
+      return {...state, photoInfo: action.selectedPhoto};
+    case ACTIONS.SET_TOPIC_DATA: 
       return "";
-    case SELECT_PHOTO: 
+    case ACTIONS.SELECT_PHOTO: 
       return "";
-    case DISPLAY_PHOTO_DETAILS: 
-      return state.displayModal = true;
+    case ACTIONS.DISPLAY_PHOTO_DETAILS: 
+      return {...state, displayModal: true};
+    case ACTIONS.CLOSE_PHOTO_DETAILS: 
+      return {...state, displayModal: false};
     default: 
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
@@ -30,41 +33,51 @@ function reducer(state, action) {
   }
 }
 
+const initialState = {
+  photoInfo: null, 
+  favorites: [], 
+  displayModal: false
+}
+
 const useApplicationData = () => {
-  const [state, dispatch] = useReducer(reducer, {photoInfo, favorites, displayModal});
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // const [photoInfo, setPhotoInfo] = useState();
   // const [favorites, setFavorites] = useState([]);
   // const [displayModal, setDisplayModal] = useState(false);
 
-  // const toggleFavorite = (photoId) => {
-  //   const isFavorite = favorites.includes(photoId);
-  //   let newFavs;
+  const toggleFavorite = (photoId) => {
+    const isFavorite = state.favorites.includes(photoId);
+    let newFavs, type;
 
-  //   if (isFavorite) {
-  //     newFavs = favorites.filter(id => id !== photoId);
-  //   } else {
-  //     newFavs = [...favorites, photoId];
-  //   }
-  //   setFavorites(newFavs);
-  // }
+    if (isFavorite) {
+      newFavs = state.favorites.filter(id => id !== photoId);
+      type = ACTIONS.FAV_PHOTO_REMOVED;
+    } else {
+      newFavs = [...state.favorites, photoId];
+      type = ACTIONS.FAV_PHOTO_ADDED
+    }
+    dispatch({type, newFavs});
+  }
 
-  // const onPhotoSelect = (selectedPhoto) => {   
-  //   setDisplayModal(true);
-  //   setPhotoInfo(selectedPhoto);
-  // }
+  const onPhotoSelect = (selectedPhoto) => {   
+    //setDisplayModal(true);
+    dispatch({type: ACTIONS.DISPLAY_PHOTO_DETAILS});
+    dispatch({type: ACTIONS.SET_PHOTO_DATA, selectedPhoto});
 
-  // const closeDisplayModal = () => {
-  //   setDisplayModal(false);
-  // }
+    //setPhotoInfo(selectedPhoto);
+  }
+
+  const closeDisplayModal = () => {
+    // setDisplayModal(false);
+    dispatch({type: ACTIONS.CLOSE_PHOTO_DETAILS});
+  }
 
   return {
-    state: {photoInfo,
-    favorites,
-    displayModal},
-    // toggleFavorite,
-    // onPhotoSelect,
-    // closeDisplayModal
+    state,
+    toggleFavorite,
+    onPhotoSelect,
+    closeDisplayModal
   };
 }
 
